6861d1f59a1480d6ddf96fd5cba4705ec35d4831
giantInterlinking <75109120+giantInterlinking@users.noreply.github.com>
2022-05-09 15:23:37 +0300

    Delete parallel directory

--- parallel/main/scala/model/entities/SpatialEntity.scala
+++
@@ -1,5 +0,0 @@
-package model.entities
-
-import org.locationtech.jts.geom.Geometry
-
-case class SpatialEntity(originalID: String, geometry: Geometry) extends Entity
--- parallel/main/scala/model/entities/geospark/SpatialEntity.scala
+++
@@ -1,6 +0,0 @@
-package model.entities.geospark
-
-import com.vividsolutions.jts.geom.Geometry
-
-case class SpatialEntity(originalID: String, geometry: Geometry) extends Entity
-
