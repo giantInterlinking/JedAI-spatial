0a8f643e95ea24874a62a799693538b4d10011b0
giantInterlinking <75109120+giantInterlinking@users.noreply.github.com>
2022-05-09 15:23:41 +0300

    Delete gui directory

--- gui/main/java/dit/anonymous/webapp/models/DatasetRepository.java
+++
@@ -1,9 +0,0 @@
-package dit.anonymous.webapp.models;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DatasetRepository extends CrudRepository<Dataset, Integer>{
-		
-	Dataset findById(int id);
-
-}
